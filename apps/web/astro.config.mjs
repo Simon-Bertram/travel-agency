d7427e1eae67f4bd6d115a71cf608ab7b62c62b6
simon-bertram <simonbertram@hotmail.com>
2026-09-08 21:08:11 +0000

    Extract brand logo into separate component

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig, envField, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: "127.0.0.1",
-  },
   env: {
     schema: {
       PUBLIC_SERVER_URL: envField.string({
@@ -16,12 +13,36 @@ export default defineConfig({
       }),
     },
   },
-  output: "server",
-  vite: {
-    plugins: [tailwindcss()],
-    optimizeDeps: {
-      exclude: ["alchemy", "@alchemy.run/frontend-frameworks"],
+  fonts: [
+    {
+      cssVariable: "--font-playfair-display",
+      name: "Playfair Display",
+      options: {
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/Playfair_Display/PlayfairDisplay-VariableFont_wght.ttf",
+            ],
+            style: "normal",
+            weight: "400 900",
+          },
+          {
+            src: [
+              "./src/assets/fonts/Playfair_Display/PlayfairDisplay-Italic-VariableFont_wght.ttf",
+            ],
+            style: "italic",
+            weight: "400 900",
+          },
+        ],
+      },
+      provider: fontProviders.local(),
     },
+  ],
+  output: "server",
+  server: {
+    host: "127.0.0.1",
+  },
+  vite: {
     environments: {
       ssr: {
         optimizeDeps: {
@@ -29,24 +50,9 @@ export default defineConfig({
         },
       },
     },
-  },
-  fonts: [{
-    provider: fontProviders.local(),
-    name: 'Playfair Display',
-    cssVariable: '--font-playfair-display',
-    options: {
-      variants: [
-        {
-          weight: "400 900",
-          style: "normal",
-          src: ["./src/assets/fonts/Playfair_Display/PlayfairDisplay-VariableFont_wght.ttf"],
-        },
-        {
-          weight: "400 900",
-          style: "italic",
-          src: ["./src/assets/fonts/Playfair_Display/PlayfairDisplay-Italic-VariableFont_wght.ttf"],
-        },
-      ],
+    optimizeDeps: {
+      exclude: ["alchemy", "@alchemy.run/frontend-frameworks"],
     },
-  }]
+    plugins: [tailwindcss()],
+  },
 });
